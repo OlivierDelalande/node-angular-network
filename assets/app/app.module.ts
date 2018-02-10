f5a59fe7c30e67dad8e853dd55ea3248c9aa1054
olivierbranch <delalande.o@g.com>
2018-02-10 13:54:41 +0100

    Preparing authentication components and adding child routing for them - Adding a form for signup using reactive data-driven forms

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import {
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import {MessageComponent} from './messages/message.component';
@@ -10,6 +13,9 @@ import {MessagesComponent} from './messages/messages.component';
 import {AuthenticationComponent} from './auth/authentication.component';
 import {HeaderComponent} from './header.component';
 import {routing} from './app.routing';
+import {LogoutComponent} from './auth/logout.component';
+import {SigninComponent} from './auth/signin.component';
+import {SignupComponent} from './auth/signup.component';
 
 @NgModule({
     declarations: [
@@ -19,9 +25,12 @@ import {routing} from './app.routing';
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComponent
+        HeaderComponent,
+        LogoutComponent,
+        SigninComponent,
+        SignupComponent
     ],
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
