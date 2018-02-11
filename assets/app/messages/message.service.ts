83a3900c311065a324123a5c9d5d52d5898dc675
olivierbranch <delalande.o@g.com>
2018-02-11 18:25:38 +0100

    updating messages and storing this update in database

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,5 +1,5 @@
 import { Http, Response, Headers } from '@angular/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import 'rxjs/Rx'
 import { Observable } from 'rxjs/Observable';
 
@@ -8,6 +8,7 @@ import { Message } from "./message.model";
 @Injectable ()
 export class MessageService {
     private messages: Message[] = [];
+    messageIsEdit = new EventEmitter<Message>();
 
     constructor(private http: Http) {}
 
@@ -15,7 +16,12 @@ export class MessageService {
         const body = JSON.stringify(message);
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('http://localhost:3000/message', body, {headers: headers})
-                   .map((response: Response) => response.json())
+                   .map((response: Response) => {
+                       const result = response.json();
+                       const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
+                       this.messages.push(message);
+                       return message;
+                   })
                    .catch((error: Response) => Observable.throw(error.json()));
     }
 
@@ -30,6 +36,18 @@ export class MessageService {
                        this.messages = transformedMessages;
                        return transformedMessages;
                    })
+                   .catch((error: Response) => Observable.throw(error.json()));
+    }
+
+    editMessage(message: Message) {
+        this.messageIsEdit.emit(message);
+    }
+
+    updateMessage(message: Message) {
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+                   .map((response: Response) => response.json())
                    .catch((error: Response) => Observable.throw(error.json()));
     }
 
