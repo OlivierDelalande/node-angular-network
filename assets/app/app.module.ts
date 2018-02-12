fbe885d9c4ea615e9f63c7a1d196e7460aa9fcb0
olivierbranch <delalande.o@g.com>
2018-02-12 16:40:24 +0100

    Add signup logic

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -17,6 +17,7 @@ import {routing} from './app.routing';
 import {LogoutComponent} from './auth/logout.component';
 import {SigninComponent} from './auth/signin.component';
 import {SignupComponent} from './auth/signup.component';
+import {AuthService} from './auth/auth.service';
 
 @NgModule({
     declarations: [
@@ -38,6 +39,7 @@ import {SignupComponent} from './auth/signup.component';
         ReactiveFormsModule,
         HttpModule
     ],
+    providers: [AuthService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
