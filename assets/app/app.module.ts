a21197873169f7830e566235c9e670509f97878e
olivierbranch <delalande.o@g.com>
2018-03-02 17:03:12 +0100

    Add error handling

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-    FormsModule,
-    ReactiveFormsModule,
-} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import {MessageComponent} from './messages/message.component';
-import {MessageListComponent} from './messages/message-list.component';
-import {MessageInputComponent} from './messages/message-input.components';
-import {MessagesComponent} from './messages/messages.component';
-import {AuthenticationComponent} from './auth/authentication.component';
-import {HeaderComponent} from './header.component';
-import {routing} from './app.routing';
-import {LogoutComponent} from './auth/logout.component';
-import {SigninComponent} from './auth/signin.component';
-import {SignupComponent} from './auth/signup.component';
-import {AuthService} from './auth/auth.service';
+import { MessageComponent } from './messages/message.component';
+import { MessageListComponent } from './messages/message-list.component';
+import { MessageInputComponent } from './messages/message-input.components';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthenticationComponent } from './auth/authentication.component';
+import { HeaderComponent } from './header.component';
+import { routing } from './app.routing';
+import { LogoutComponent } from './auth/logout.component';
+import { SigninComponent } from './auth/signin.component';
+import { SignupComponent } from './auth/signup.component';
+import { AuthService } from './auth/auth.service';
+import { ErrorComponent } from './errors/error.component';
+import {ErrorService} from './errors/error.service';
 
 @NgModule({
     declarations: [
@@ -30,7 +29,8 @@ import {AuthService} from './auth/auth.service';
         HeaderComponent,
         LogoutComponent,
         SigninComponent,
-        SignupComponent
+        SignupComponent,
+        ErrorComponent
     ],
     imports: [
         BrowserModule,
@@ -39,7 +39,7 @@ import {AuthService} from './auth/auth.service';
         ReactiveFormsModule,
         HttpModule
     ],
-    providers: [AuthService],
+    providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
